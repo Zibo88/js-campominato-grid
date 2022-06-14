764529627d800723a21b6548227c69f8fc5b7d00
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-14 17:45:20 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -18,10 +18,10 @@ let dificultLevel = parseInt(prompt('dimmi il livell odi difficltà tra 1-2-3'))
 // console.log(dificultLevel)
 
 let userNumber = parseInt (prompt('inserisci un numero'))
+console.log("numeri dati dall'utente", userNumber)
 
 // attribuisco una condizione con una variabile range 
 // se sceglie 1: range 1-100, 2: range 1-81, 3 range 1-49;
-
 let rangeMax;
 if(dificultLevel === 1){
     rangeMax = 100;
@@ -30,44 +30,79 @@ if(dificultLevel === 1){
 } else if(dificultLevel === 3){
     rangeMax = 49;
 }
-
 console.log('Questo è il numero massimo di numeri', rangeMax)
 
 // Genero le bombe
-// genero 16 numeri in base al numero massimo del livello di riferimento (gameMaxNumber)
-    // individuo il numero massimo di tentativi che sarà gameMaxNumber - 16
-
+// genero 16 numeri in base al numero di bombe da generare, valore minimo e valore massimo del livello di riferimento (rangeMax)
     // evoco la funzione e salvo in una variabile
 let theBombs = generateBombs( 16, 1, rangeMax);
-// console.log(theBombs)
+console.log("numero di bombe in base all'array the bombs", theBombs)
 
+// tentativi possibili
+// let userAttempts = rangeMax - 16;
+let userAttempts = 2;
+console.log(userAttempts);
+
+
+// definisco quando termina il gioco e se vince o se perde creando un ciclo while
+//creo un array che faccia da contenitore dei numeri dati dall'utente
+let gamePlay = true;
+
+let arrayUserNumber = [];
+
+while(gamePlay === true){
+    // richiedo il numero all'utente finchè..
+    let userNumber = parseInt (prompt('inserisci un numero'));
+
+    
+    // se il numero generato include il numero dell'utente
+    if(theBombs.includes(userNumber)){
+        // il gioco termina
+        gamePlay = false
+        alert('Hai perso')
+
+        // altrimenti
+    } else {
+        // se l'array vuoto non continiene usernumber 
+        if(!arrayUserNumber.includes(userNumber)){
+            // lo pushamo
+            arrayUserNumber.push(userNumber);
+        }
+
+        // definiamo la vittoria
+        if (arrayUserNumber.length === userAttempts){
+            alert('hai vinto')
+        }
+    }
+}
+
+// controllo dell'array inizialmente vuoto
+// console.log(arrayUserNumber)
+
+
+//---------
+// FUNCTION
+//---------
 // creo una funzione che generai numeri random per le bombe
 function generateBombs (numberOfBombs, numMin, numMax){
-    // creo un array vuoto
+    // creo un array vuoto dove inserire i numeri che verrnno generati
     let numberGenerateArray = [];
     
-    
-
-    // creo un ciclo for che vada avanti per il numero di bombe richiesto
-    for(let i = 0; i < numberOfBombs; i++){
+    // creo un ciclo while che vada avanti per tutta la lunghezza dell'array che deve essere inferiore al numero di bombe
+    while(numberGenerateArray.length < numberOfBombs){
         // evoco la funzione random e la salvo in una variabile
         let randomNumber = getRndInteger(numMin, numMax);
-        console.log(randomNumber)
+        // console.log(randomNumber)
         // pusho solo i numeri che non sono gia presenti nell'array
        if(!numberGenerateArray.includes(randomNumber)){
             numberGenerateArray.push(randomNumber);
        }  
 
-    }
-       
+    }  
     console.log(numberGenerateArray)
 
-
-    return numberGenerateArray
-
-    
+    return numberGenerateArray  
 }
-
 
 
 // creo una funzione che generi numeri random
@@ -75,15 +110,15 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
 
+
+
 // Parte logica
 
 //il gioco andrà avanti finchè:
     // il giocatore non sceglie un numero bomba
     // completa tutti i tentativi senza sbagliare
 
-//------------
-// FUNCTION
-//------------
+
 
 
 
