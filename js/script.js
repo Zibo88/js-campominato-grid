4cb39ba1099704dba4d94536fe2bc7aa015ce675
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-14 16:14:18 +0200

    creo una funzione che generi numeri random e inserisca solo quelli non gia presenti

--- js/script.js
+++ js/script.js
@@ -17,8 +17,11 @@
 let dificultLevel = parseInt(prompt('dimmi il livell odi difficltà tra 1-2-3'));
 // console.log(dificultLevel)
 
+let userNumber = parseInt (prompt('inserisci un numero'))
+
 // attribuisco una condizione con una variabile range 
 // se sceglie 1: range 1-100, 2: range 1-81, 3 range 1-49;
+
 let rangeMax;
 if(dificultLevel === 1){
     rangeMax = 100;
@@ -28,7 +31,7 @@ if(dificultLevel === 1){
     rangeMax = 49;
 }
 
-console.log(rangeMax)
+console.log('Questo è il numero massimo di numeri', rangeMax)
 
 // Genero le bombe
 // genero 16 numeri in base al numero massimo del livello di riferimento (gameMaxNumber)
@@ -38,14 +41,39 @@ console.log(rangeMax)
 let theBombs = generateBombs( 16, 1, rangeMax);
 // console.log(theBombs)
 
+// creo una funzione che generai numeri random per le bombe
 function generateBombs (numberOfBombs, numMin, numMax){
+    // creo un array vuoto
+    let numberGenerateArray = [];
+    
+    
 
-    console.log(numberOfBombs);
-    console.log(numMin);
-    comsole.log(numMax);
+    // creo un ciclo for che vada avanti per il numero di bombe richiesto
+    for(let i = 0; i < numberOfBombs; i++){
+        // evoco la funzione random e la salvo in una variabile
+        let randomNumber = getRndInteger(numMin, numMax);
+        console.log(randomNumber)
+        // pusho solo i numeri che non sono gia presenti nell'array
+       if(!numberGenerateArray.includes(randomNumber)){
+            numberGenerateArray.push(randomNumber);
+       }  
+
+    }
+       
+    console.log(numberGenerateArray)
 
 
+    return numberGenerateArray
+
+    
 }
+
+
+
+// creo una funzione che generi numeri random
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 
 // Parte logica
 
